4238f25456b5873f41ee9121b698918eefe18151
Resetwyl <121613205+Resetwyl@users.noreply.github.com>
2022-12-29 20:14:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var S = {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
       // 倒计时修改这句中的数字，不需要倒计时删除“|#countdown 3|”，换内容修改文字
-      S.UI.simulate('|#countdown 3|祝|石珂棋|2023|新年快乐|心想事成|考试圆满|#rectangle|');
+      S.UI.simulate('|#countdown 3|祝|石珂棋|新年快乐|2023|心想事成|考试圆满|#rectangle|');
     }
 
     S.Drawing.loop(function () {
